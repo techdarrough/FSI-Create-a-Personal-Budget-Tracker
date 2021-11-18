fe386883fe3291d91e9c5548c5b368fb44901a80
Preston Darrough <techdarrough@gmail.com>
2021-11-17 17:55:53 -0800

    went back to for loops

--- index.js
+++ index.js
@@ -5,45 +5,45 @@ let annualExpenses = 0
 // write code that itterates over each var to set all expenses
 
 
-let i = 0
-while (i < weeklyExpenseQuestions.lenght) {
-    i++;
-    let answer = window.prompt(weeklyExpenseQuestions[i])
-    weeklyExpenses = weeklyExpenses + parseInt(answer)
+// let i = 0
+// while (i < weeklyExpenseQuestions.lenght) {
+//     i++;
+//     let answer = window.prompt(weeklyExpenseQuestions[i])
+//     weeklyExpenses = weeklyExpenses + parseInt(answer)
     
     
-}
-while (i < monthlyExpenseQuestions.lenght) {
-    i++;
-    let answer = window.prompt(monthlyExpenseQuestions[i])
-    monthlyExpenses = monthlyExpenses + parseInt(answer)
-    
-    
-}
-while (i < annualExpenseQuestions.lenght) {
-    i++;
-    let answer = window.prompt(annualExpenseQuestions[i])
-    annualExpenses = annualExpenses + parseInt(answer)
-    
-    
-}
-
-
-
-
-// for (let i = 0; i < weeklyExpenseQuestions.length; i++) {
-// let answer = window.prompt(weeklyExpenseQuestions[i]);
-// weeklyExpenses = weeklyExpenses + parseInt(answer)
-
 // }
-
-// for (let i = 0; i < monthlyExpenseQuestions.length; i++) {
-//     let answer = window.prompt(monthlyExpenseQuestions[i]);
+// while (i < monthlyExpenseQuestions.lenght) {
+//     i++;
+//     let answer = window.prompt(monthlyExpenseQuestions[i])
 //     monthlyExpenses = monthlyExpenses + parseInt(answer)
+    
+    
+// }
+// while (i < annualExpenseQuestions.lenght) {
+//     i++;
+//     let answer = window.prompt(annualExpenseQuestions[i])
+//     annualExpenses = annualExpenses + parseInt(answer)
+    
+    
 // }
 
-// for (let i = 0; i < annualExpenseQuestions.length; i++) {
-//     let answer = window.prompt(annualExpenseQuestions[i]);
-//     annualExpenses = annualExpenses + parseInt(answer)
-// }
+
+
+
+for (let i = 0; i < weeklyExpenseQuestions.length; i++) {
+let answer = window.prompt(weeklyExpenseQuestions[i]);
+weeklyExpenses = weeklyExpenses + parseInt(answer)
+
+}
+
+for (let i = 0; i < monthlyExpenseQuestions.length; i++) {
+    let answer = window.prompt(monthlyExpenseQuestions[i]);
+    monthlyExpenses = monthlyExpenses + parseInt(answer)
+}
+
+for (let i = 0; i < annualExpenseQuestions.length; i++) {
+    let answer = window.prompt(annualExpenseQuestions[i]);
+    annualExpenses = annualExpenses + parseInt(answer)
+}
 
